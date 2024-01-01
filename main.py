2cc6ece7464708ba9fd8a0fa3c56f888c7db88ec
Vladyslav <47224640+vadosich38@users.noreply.github.com>
2024-01-01 18:05:53 +0100

    middleware hotfix

--- main.py
+++ main.py
@@ -48,7 +48,7 @@ async def main() -> None:
     my_disp.include_routers(start_router, help_cmd_router, admin_cmd_router, delete_admin_router, new_admin_router,
                             send_cmd_router, status_cmd_router)
     # регистрируем middleware в диспетчере сразу для всех обновлений (событий)
-    my_disp.update.outer_middleware.register(MyMiddleWare)
+    my_disp.update.outer_middleware.register(MyMiddleWare())
 
     async with my_bot.context():
         await my_bot.delete_webhook(drop_pending_updates=True)
--- middlware/middleware.py
+++ middlware/middleware.py
@@ -1,6 +1,7 @@
 from typing import Any, Awaitable, Callable, Dict, Union
 
 from aiogram.types import Message, CallbackQuery
+from aiogram.types import TelegramObject
 from aiogram.dispatcher.middlewares.base import BaseMiddleware
 from aiogram.dispatcher.middlewares.error import CancelHandler
 
@@ -13,12 +14,13 @@ class MyMiddleWare(BaseMiddleware):
     """
     async def __call__(
             self,
-            handler: Callable[[Message, Dict[str, Any]], Awaitable[Any]],
+            handler: Callable[[TelegramObject, Dict[str, Any]], Awaitable[Any]],
             event: Union[Message, CallbackQuery],
             data: Dict[str, Any]
     ) -> Any:
         print("\nОтправлено сообщение в чате типа:", event.chat.type)
         if event.chat.type != "private":
             print("Сообщение или команда не будет обработаны, так как вызваны не в личном чате!")
-            raise CancelHandler
+            return
+            # raise CancelHandler
         return await handler(event, data)
